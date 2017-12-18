65f954cd0d5d50a11f9c64cb6069adf43d6ba78d
Mustafa Abdi <mustafabyabdi@gmail.com>
2017-12-18 17:53:59 -0500

    Removed extra spaces

--- vscoscrape/vscoscrape.py
+++ vscoscrape/vscoscrape.py
@@ -63,56 +63,37 @@ class Scraper(object):
         videos = []
         images = []
         videx = 0
-
-
         total = 0
         count = len(r.json()["media"])
         page = 1
-
-
         while count > 0:
-        
             r = self.session.get(self.mediaurl,headers=constants.media,params = {"page":"%s"%page})
             page+=1                 
             count = len(r.json()["media"])
-
             total += count
-                
-                
-                
-
         r = self.session.get(self.mediaurl,headers=constants.media,params = {"size":"%s"%total})
         red = r.json()["media"]
         for vid in tqdm(red,total=total):
             if vid['is_video'] is True:
                 if '%s.mp4' % str(vid["upload_date"])[:-3] in os.listdir():
                     videx +=1
-
                     continue
                 videx +=1
-
                 vid_indiv = "http://%s"% vid["video_url"]
                 videos.append(vid_indiv)
                 with open('%s.mp4'%str(vid["upload_date"])[:-3],'wb') as f:
                     for chunk in requests.get(vid_indiv ,stream=True).iter_content(chunk_size=1024): 
                         if chunk:
                             f.write(chunk)
-
             if vid['is_video'] is False:
                 if '%s.jpg' % str(vid["upload_date"])[:-3] in os.listdir():
                     videx +=1
-
                     continue
                 videx += 1
- 
                 img_indiv = "http://%s"% vid["responsive_url"]
                 images.append(img_indiv)
                 with open('%s.jpg'%str(vid["upload_date"])[:-3],'wb') as f:
                     f.write(requests.get(img_indiv ,stream=True).content)
-
-            
-            
-
         print("Videos and Images of %s downloaded"%self.username)
 
 
@@ -120,7 +101,6 @@ class Scraper(object):
         page=1
         r = self.session.get(self.mediaurl,headers=constants.media,params = {"page":"%s"%page}) 
         count = len(r.json()["media"])
-
         coords = []
         index = 0
         indexs = []
@@ -128,8 +108,6 @@ class Scraper(object):
             for loc in r.json()["media"]:
                 if loc["has_location"]:
                     coords.append([loc["location_coords"][1],loc["location_coords"][0],datetime.datetime.fromtimestamp(int(loc["capture_date"])/1000).strftime('%Y-%m-%d %I:%M:%S %p')])
-                    
-
                     index +=1
                     indexs.append(index)
                 else:
@@ -138,9 +116,6 @@ class Scraper(object):
             page+=1                 
             r = self.session.get(self.mediaurl,headers=constants.media,params = {"page":"%s"%page})
             count = len(r.json()["media"])
-
-
-
         gmap = gmplot.GoogleMapPlotter(39.106506,-77.555574,13)
         gmap.coloricon = 'file:///' + os.path.dirname(gmplot.__file__).replace('\\', '/') + '/markers/%s.png'
         index = 0
@@ -156,94 +131,59 @@ class Scraper(object):
                 else:
                     time.sleep(.3)
             gmap.marker(coord[0],coord[1], '#3B0B39','#E0FFFF', title="%s at %s" %(g,coord[2]))
-
             time.sleep(.15)
-
-
         gmap.draw("markers.html")
-
-        return gmap
-
-            
+        return gmap        
     def getJournal(self):
         page = 1
         r = self.session.get(self.journalurl,headers=constants.media,params = {"page":"%s"%page})
         count = len(r.json()["articles"])
         videx = 0
-
         total = 0
-
-
-
-        while count > 0:
-
-                
+        while count > 0:    
             r = self.session.get(self.journalurl,headers=constants.media,params = {"page":"%s"%page})
             page+=1                 
-
-
             for x in r.json()["articles"]:
-                total += len(x["body"])
-                
-            count = len(r.json()["articles"])
-            
+                total += len(x["body"])  
+            count = len(r.json()["articles"])   
         path = "%s\journal" % (os.getcwd())
         if not os.path.exists(path):
             os.makedirs(path)
         os.chdir(path)
-
         pbar = tqdm(total=total)
-
-        r = self.session.get(self.journalurl,headers=constants.media,params = {"size":"%s"%total})
-        
+        r = self.session.get(self.journalurl,headers=constants.media,params = {"size":"%s"%total}) 
         for j in r.json()["articles"]:
-
             path = "%s\%s"% (os.getcwd(),str(j["permalink"]))
             if not os.path.exists(path):
                 os.makedirs(path)
             os.chdir(path)
             for img in j["body"]:
-
                 if img["type"] == "video":
                     if '%s.mp4' % img["content"][0]["id"] in os.listdir():
                         videx +=1
-
                         pbar.update(1)
                         continue
-                    videx +=1
-
-                    
+                    videx +=1            
                     vid_indiv = "http://%s"% img["content"][0]["responsive_url"]
                     pbar.update(1)
                     with open('%s.mp4'%img["content"][0]["id"],'wb') as f:
                         for chunk in requests.get(vid_indiv ,stream=True).iter_content(chunk_size=1024): 
-                            if chunk:
-                                
+                            if chunk:                         
                                 f.write(chunk)
-
                 if img["type"] == "image":
                     if '%s.jpg' % img["content"][0]["id"] in os.listdir():
                         videx +=1
-
                         pbar.update(1)
                         continue
                     videx += 1
-
                     img_indiv = "http://%s"% img["content"][0]["responsive_url"]
                     pbar.update(1)
                     with open('%s.jpg'%img["content"][0]["id"],'wb') as f:
-                        f.write(requests.get(img_indiv ,stream=True).content)
-                    
+                        f.write(requests.get(img_indiv ,stream=True).content)       
             os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
         pbar.close()
 
                 
-
-
-        
-        
-
-
 
 def main():
     parser = argparse.ArgumentParser(
